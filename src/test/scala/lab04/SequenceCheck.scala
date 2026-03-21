da61ee88ffab79fd016b8e8f4e5958f3dbfb35d3
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2026-03-22 00:11:44 +0100

    exercise done

--- src/test/scala/lab04/SequenceCheck.scala
+++ src/test/scala/lab04/SequenceCheck.scala
@@ -20,6 +20,7 @@ object SequenceCheck extends Properties("Sequence"):
   // define custom arbitrary lists and mappers
   given intSeqArbitrary: Arbitrary[Sequence[Int]] = Arbitrary(sequenceGen[Int]())
   given mapperArbitrary: Arbitrary[Int => Int] = Arbitrary(Gen.oneOf[Int => Int]( _+1, _*2, x => x*x))
+  given predicateArbitrary: Arbitrary[Int => Boolean] = Arbitrary(Gen.oneOf[Int => Boolean]( _ % 2 == 0, _ > 5, _ < 3))
 
   // check axioms, universally
   property("mapAxioms") =
@@ -28,6 +29,18 @@ object SequenceCheck extends Properties("Sequence"):
       (seq, f) match
         case (Nil(), f) =>  map(Nil())(f) == Nil()
         case (Cons(h, t), f) => map(Cons(h, t))(f) == Cons(f(h), map(t)(f))
+
+  property("sumAxioms") =
+    forAll: (seq: Sequence[Int]) =>
+      seq match
+        case Nil() => sum(Nil()) == 0
+        case Cons(h, t) => sum(Cons(h, t)) == h + sum(t)
+
+  property("filterAxioms") =
+    forAll: (seq: Sequence[Int], f: Int => Boolean) =>
+      seq match
+        case Nil() => filter(Nil())(f) == Nil()
+        case Cons(h, t) => filter(Cons(h, t))(f) == (if f(h) then Cons(h, filter(t)(f)) else filter(t)(f))
 
   // how to check a generator works as expected
   @main def showSequences() =
